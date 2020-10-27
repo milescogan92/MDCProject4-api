9ab411f796548428f0ee353f09f4060a08425cc5
milescogan92 <miles.cogan92@gmail.com>
2020-10-27 13:58:44 -0400

    Created resource
    Created initial model for car repairs in app/models/repair.js

---
+++ app/models/repair.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const repairSchema = new mongoose.Schema({
+  date: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: String,
+    required: true
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps: true
+})
+
+module.exports = mongoose.model('Repair', repairSchema)
